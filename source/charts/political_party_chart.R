084f006f853ee0e3bd10b3c82642878d5078e9e2
Ji_yoon_KIM <wbsk0810@naver.com>
2022-11-14 15:33:21 -0800

    made edits

--- source/charts/political_party_chart.R
+++ source/charts/political_party_chart.R
@@ -6,16 +6,19 @@ library("tidyverse")
 
 #import and organize dataset
 hate_crime <- read.csv(file = 'data/hate_crime.csv') %>%
-  rename(year = DATA_YEAR) %>%
-  rename(state = STATE_NAME) %>%
-  rename(state_po = STATE_ABBR) %>%
+  rename(year = DATA_YEAR,
+         state = STATE_NAME,
+         state_po = STATE_ABBR) %>%
   filter(year == 2020)
 
 political_party <- read.csv(file = 'data/political_party.csv') %>%
   rename(party = party_simplified) %>%
-  mutate(state = str_to_title(state)) %>%
-  mutate(party = str_to_title(party)) %>%
+  mutate(state = str_to_title(state),
+         party = str_to_title(party)) %>%
   filter(year == 2020)
+
+us_population_2020 <- read.csv(file = 'data/US_Population_2020.csv')
+
 
 #how many hate crimes in each state
 state_crimes <- hate_crime %>%
@@ -25,9 +28,7 @@ state_crimes <- hate_crime %>%
 #calculating party affiliation by state
 state_by_votes_2020 <- political_party %>%
   group_by(state) %>%
-  filter(candidatevotes == max(candidatevotes, na.rm = TRUE)) %>%
-  
-  
+  filter(candidatevotes == max(candidatevotes, na.rm = TRUE))
 
 #join dataset
 
@@ -38,7 +39,6 @@ state_by_votes_2020 <- political_party %>%
 #What am I gonna make?
 # in 2020, party affiliation by state & hate crime rates. 
 # calculate ratio!
-
 
 # dem / re
 # ----- | -----
