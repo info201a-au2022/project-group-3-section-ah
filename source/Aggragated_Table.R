ace7eea9f4ecf0ca84eb15adabec8ff67a8d6588
cinnammj <114957844+cinnammj@users.noreply.github.com>
2022-11-14 20:46:39 -0800

    added library(tidyverse)

--- source/Aggragated_Table.R
+++ source/Aggragated_Table.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 hatecrime <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-3-section-ah/main/data/hate_crime.csv")
 hatecrime <- hatecrime %>% group_by(BIAS_DESC)
 
