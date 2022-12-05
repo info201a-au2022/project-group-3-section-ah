1c41e7f50963ad26c24f51f93a98448fc8d54330
cinnammj <114957844+cinnammj@users.noreply.github.com>
2022-12-04 16:32:28 -0800

    added my part to ui (Michael)

--- source/app_ui.R
+++ source/app_ui.R
@@ -36,7 +36,24 @@ interactive_page <- tabPanel(
 )
 
 # Interactive page 2 - Michael
+Takeaways <- tabPanel(
+  "Key Takeaways",
+  h2("The Three Key Takeaways"),
+  p("1.) Although the 50 states party affiliation is 50% Democrat + D.C and 50% Republican, By looking at the chart, we can see that the states that voted for the Republican party is darker, While the states that voted for the Democrats are generally lighter in color which means less hate crime"),
+  
+  p("2.) Most states have punishments for Hate crimes, excluding Arkansas, Indiana, South Carolina, and Wyoming. and although most states have laws against things such as race, religion, and ethnicity. Some states start to dwindle on Disability, Gender, Sexual Orientation, and most of all Gender Identity."), 
+  
+  p("3.) The top 3 Hate crimes (documented) that were committed were Anti-Black or African American, Anti-Gay (Male), and Anti-Jewish. Which means that Racism, Anti-Semitism, and Homophobia are still prevalent, however that doesn't mean only those things exist, overall hatred is too big of a problem in our country")
+)
 
+
+map_sidebar_content <- sidebarPanel(selectInput("mapvar", label = "Variable to Map", 
+                                                choices = list("Race, Religion, and Ethnicity" = "Race.Religion.Ethnicity","Disability" = "Disability"
+                                                               ,"Gender" = "Gender", "Sexual Orientation" = "Sexual.Orientation", "Gender Identity" = "Gender.Identity")))
+
+map_main_content <- mainPanel(plotlyOutput("map"))
+
+map_panel <- tabPanel("Map",titlePanel("Protection by Identifier"), sidebarLayout(map_sidebar_content, map_main_content))
 # Interactive page 3 - political party (Jiyoon)
 interactive_political_party <- tabPanel(
   "Political Party",
@@ -63,9 +80,9 @@ ui <- navbarPage(
   titlePanel("Hate Crimes in the U.S."),
   introduction_page,
   interactive_page, #qiqi's
-  #michael's interactive page goes here
+  map_panel, #michael's interactive page goes here
   interactive_political_party, #Jiyoon's interactive page
-  
+  Takeaways, #michael's takeaways
 )
 
 
